d9d8aba748c56707dcc223bfb5bbecf10ba9a916
meng sokheng <mengsokheng143@gmail.com>
2025-12-18 09:27:02 +0700

    adding whitenoise settings

--- BackendPOS/config/settings.py
+++ BackendPOS/config/settings.py
@@ -101,7 +101,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "config.urls"
 
