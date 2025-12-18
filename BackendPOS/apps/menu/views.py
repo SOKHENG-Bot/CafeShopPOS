eda77e21b202d9514d4c1445caea9605926dac4f
meng sokheng <mengsokheng143@gmail.com>
2025-12-18 20:31:07 +0700

    Final update cloudary

--- BackendPOS/apps/menu/views.py
+++ BackendPOS/apps/menu/views.py
@@ -1,9 +1,6 @@
 from rest_framework import permissions, viewsets
 from .models import Item, Category
 from .serializers import MenuItemSerializer, CategorySerializer
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -20,18 +17,3 @@ class ItemsViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     search_fields = ["name", "description"]
     ordering_fields = ["price", "created_at"]
-
-    def create(self, request, *args, **kwargs):
-        logger.info(f"Creating menu item. Image in request: {'image' in request.FILES}")
-        if 'image' in request.FILES:
-            logger.info(f"Image file: {request.FILES['image'].name}, size: {request.FILES['image'].size}")
-
-        response = super().create(request, *args, **kwargs)
-
-        if response.status_code == 201:
-            item_data = response.data
-            logger.info(f"Created item with image_url: {item_data.get('image_url')}")
-            logger.info(f"Raw image field: {item_data.get('image')}")
-            logger.info(f"Full response data: {item_data}")
-
-        return response
