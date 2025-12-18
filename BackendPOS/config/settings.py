02fa344582ad96edadc904bd52d54b56e82a95e0
meng sokheng <mengsokheng143@gmail.com>
2025-12-18 14:10:36 +0700

    update media storage for Railway

--- BackendPOS/config/settings.py
+++ BackendPOS/config/settings.py
@@ -184,7 +184,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
