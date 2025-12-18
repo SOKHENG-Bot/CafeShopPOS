c6cb695cb4eb9181ad3976471af29ae2f4756cfa
meng sokheng <mengsokheng143@gmail.com>
2025-12-18 19:48:03 +0700

    update image cloudinary

--- BackendPOS/apps/menu/models.py
+++ BackendPOS/apps/menu/models.py
@@ -40,7 +40,7 @@ class Item(models.Model):
     description = models.TextField(blank=True, max_length=30)
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
     image = models.ImageField(upload_to=menu_image_upload_path, null=True, blank=True)
-    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True, blank=True, related_name="items")
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, null=True, blank=True, related_name="items")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
