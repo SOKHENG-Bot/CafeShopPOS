83ca074e2fdc204f1bce790467bb3de59498fc76
meng sokheng <mengsokheng143@gmail.com>
2025-12-18 09:57:47 +0700

    fix secretKey error

--- BackendPOS/config/settings.py
+++ BackendPOS/config/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
-from decouple import config
+
+# from decouple import config
 import os
 
 load_dotenv()
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-r6!)vb&xlbd81s@yhthqgw)#rjz3b@(xrjr4=7iziq*%d=g@!2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
